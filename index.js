5ed8c2bf19c93dad34da578723b6a7dea03388f7
Zuza Napiórkowska <napiorkowskaz@gmail.com>
2022-02-22 22:15:02 +0100

    add fetchm with refactoring

--- index.js
+++ index.js
@@ -1,28 +1,85 @@
 
-const firstReq = new XMLHttpRequest();
-firstReq.addEventListener("load", function() {
-    const data = JSON.parse(this.responseText);
+// const firstReq = new XMLHttpRequest();
+// firstReq.addEventListener("load", function() {
+//     const data = JSON.parse(this.responseText);
+//     for (let planet of data.results) {
+//         console.log(planet.name);
+//     }
+//     const filmURL = data.results[0].films[0];
+//     const filmReq = new XMLHttpRequest();
+//     filmReq.addEventListener("load", function(){
+//         const filmData = JSON.parse(this.responseText);
+//         console.log("Here's the film data:  ", filmData);
+//         console.log("okiiii");
+//     })
+//     filmReq.addEventListener("error", function(){
+//         console.log("nieoki");
+//     })
+//     filmReq.open("GET", filmURL);
+//     filmReq.send();
+// })
+// firstReq.addEventListener("error", ()=> {
+//     console.log("error :(");
+// })
+
+// firstReq.open("GET", "https://swapi.dev/api/planets/")
+// firstReq.send();
+
+// console.log("Request sent");
+
+//........................................................
+
+function checkStatusAndParse(response) {
+    if (response.ok) {
+        return response.json()
+    } else {
+        console.log("not status 200, instead ", response.status);
+        throw new Error(`Current status: ${response.status}`)
+    }
+}
+
+// fetch("https://swapi.dev/api/planets/")
+//     .then(checkStatusAndParse)
+//     .then((data) => {
+//         const filmURL = data.results[0].films[0];
+//         return fetch(filmURL)
+//     })
+//     .then(checkStatusAndParse)
+//     .then((data) => {
+//         const director = data.director
+//         console.log(director);
+//     }).catch((err) => {
+//         console.log("sth went wrong", err);
+//     })
+
+//.........................................................................................
+function printPlanets(data) {
+    console.log("Loaded 10 more planets");
     for (let planet of data.results) {
         console.log(planet.name);
     }
-    const filmURL = data.results[0].films[0];
-    const filmReq = new XMLHttpRequest();
-    filmReq.addEventListener("load", function(){
-        const filmData = JSON.parse(this.responseText);
-        console.log("Here's the film data:  ", filmData);
-        console.log("okiiii");
-    })
-    filmReq.addEventListener("error", function(){
-        console.log("nieoki");
-    })
-    filmReq.open("GET", filmURL);
-    filmReq.send();
-})
-firstReq.addEventListener("error", ()=> {
-    console.log("error :(");
-})
+    return Promise.resolve(data.next);
+    // takie cuś jak powyżej da nam resolved promise'a
+}
 
-firstReq.open("GET", "https://swapi.dev/api/planets/")
-firstReq.send();
+function getNext10planets(url) {
+    return fetch(url);
+}
 
-console.log("Request sent");
+    fetch("https://swapi.dev/api/planets/")
+    .then(checkStatusAndParse)
+    .then(printPlanets)
+    .then(getNext10planets)
+    .then(checkStatusAndParse)
+    .then(printPlanets)
+    .then(getNext10planets)
+    .then(checkStatusAndParse)
+    .then(printPlanets)
+    .then(getNext10planets)
+    .then(checkStatusAndParse)
+    .then(printPlanets)
+    .catch((err) => {
+        console.log("sth went wrong", err);
+    })
+
+//....................................................
